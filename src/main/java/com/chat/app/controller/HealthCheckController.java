255d7bc61c95c61970321352a070ad8c1ea75a03
Mukesh Patel <Amukeshpatel222@gmail.com>
2026-01-11 11:55:27 +0530

    Add HealthCheckController to fix 404 error on root URL

---
+++ src/main/java/com/chat/app/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.chat.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public String home() {
+        return "Chat Server is Running!";
+    }
+}
